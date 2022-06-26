ce05918102e91a2fefd7104bd82ca8bd2b7201f5
エトワール <74129373+Etoile1024@users.noreply.github.com>
2022-06-26 18:14:16 +0800

    Add file

---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            //openFile.DefaultExt = "mp4";
+            openFile.Filter = "Video file (*.mp4)|*.mp4|Data file (*.dat)|*.dat";
+            openFile.ShowDialog();
+            textBox1.Text = openFile.FileName;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] header = new byte[] { 0x00, 0x00, 0x00, 0x20, 0x66, 0x74, 0x79, 0x70, 0x6D, 0x70, 0x34, 0x32 };
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("路徑唔可以冇野！", "錯誤：", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!File.Exists(textBox1.Text))
+            {
+                MessageBox.Show("搵唔到個file wor!", "錯誤：", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (textBox1.Text.EndsWith(".mp4"))
+            {
+                if (!Directory.Exists("output"))
+                {
+                    Directory.CreateDirectory("output");
+                }
+                button2.Enabled = false;
+                byte[] buffer = File.ReadAllBytes(textBox1.Text);
+                BoyerMoore boyerMoore = new BoyerMoore(header);
+                //boyerMoore.SetPattern(header);
+                var result = boyerMoore.SearchAll(buffer);
+                progressBar1.Value = 0;
+                progressBar1.Maximum = result.Count;
+                for (int i = 0; i < result.Count; i++)
+                {
+                    using (BinaryReader binaryReader = new BinaryReader(new MemoryStream(buffer)))
+                    {
+                        int length = i + 1 != result.Count ? result[i + 1] - result[i] : buffer.Length - result[i];
+                        binaryReader.BaseStream.Position = result[i];
+                        using (BinaryWriter binaryWriter = new BinaryWriter(new FileStream($"output\\output_{i}.mp4", FileMode.Create)))
+                        {
+                            byte[] partofbuffer = binaryReader.ReadBytes(length);
+                            binaryWriter.Write(partofbuffer);
+                        }
+                    }
+                    progressBar1.Value++;
+                }
+                MessageBox.Show("拆完了！", "訊息：", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button2.Enabled = true;
+            }
+            if (textBox1.Text.EndsWith(".dat"))
+            {
+                button2.Enabled = false;
+                progressBar1.Maximum = 0;
+                PRead pRead = new PRead(textBox1.Text);
+                progressBar1.Maximum = pRead.ti.Count;
+                foreach (string filename in pRead.ti.Keys)
+                {
+                    string directory = Path.GetDirectoryName($"output\\{filename}");
+                    if (!Directory.Exists(directory))
+                    {
+                        Directory.CreateDirectory(directory);
+                    }
+                    using (BinaryWriter binaryWriter = new BinaryWriter(new FileStream($"output\\{filename}", FileMode.Create)))
+                    {
+                        byte[] buffer = pRead.Data(filename);
+                        binaryWriter.Write(buffer);
+                    }
+                    progressBar1.Value++;
+                }
+                MessageBox.Show("Unpacked!", "Info:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button2.Enabled = true;
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp5/PRead.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp5
+{
+    public class PRead
+    {
+        public PRead(string fn)
+        {
+            this.fs = new FileStream(fn, FileMode.Open, FileAccess.Read);
+            this.Init();
+            if (fn.ToLower().EndsWith("adult.dat"))
+            {
+                this.ti.Remove("def/version.txt");
+            }
+        }
+
+        // Token: 0x06000037 RID: 55 RVA: 0x00003494 File Offset: 0x00001894
+        public PRead()
+        {
+            this.fs.Close();
+        }
+
+        // Token: 0x06000038 RID: 56 RVA: 0x000034C8 File Offset: 0x000018C8
+        private void Init()
+        {
+            this.ti = new Dictionary<string, PRead.fe>();
+            this.fs.Position = 0L;
+            byte[] array = new byte[1024];
+            this.fs.Read(array, 0, 1024);
+            int num = 0;
+            for (int i = 4; i < 255; i++)
+            {
+                num += BitConverter.ToInt32(array, i * 4);
+            }
+            byte[] array2 = new byte[16 * num];
+            this.fs.Read(array2, 0, array2.Length);
+            this.dd(array2, 16 * num, BitConverter.ToUInt32(array, 212));
+            int num2 = BitConverter.ToInt32(array2, 12);
+            int num3 = num2 - (1024 + 16 * num);
+            byte[] array3 = new byte[num3];
+            this.fs.Read(array3, 0, array3.Length);
+            this.dd(array3, num3, BitConverter.ToUInt32(array, 92));
+            int num4 = 0;
+            for (int j = 0; j < num; j++)
+            {
+                int num5 = 16 * j;
+                uint l = BitConverter.ToUInt32(array2, num5);
+                int num6 = BitConverter.ToInt32(array2, num5 + 4);
+                uint k = BitConverter.ToUInt32(array2, num5 + 8);
+                uint p = BitConverter.ToUInt32(array2, num5 + 12);
+                int m;
+                for (m = num6; m < array3.Length; m++)
+                {
+                    if (array3[m] == 0)
+                    {
+                        break;
+                    }
+                }
+                string key = Encoding.ASCII.GetString(array3, num4, m - num4).ToLower();
+                PRead.fe value = default(PRead.fe);
+                value.p = p;
+                value.L = l;
+                value.k = k;
+                this.ti.Add(key, value);
+                num4 = m + 1;
+            }
+        }
+
+        // Token: 0x06000039 RID: 57 RVA: 0x00003670 File Offset: 0x00001A70
+        private void gk(byte[] b, uint k0)
+        {
+            uint num = k0 * 7391u + 42828u;
+            uint num2 = num << 17 ^ num;
+            for (int i = 0; i < 256; i++)
+            {
+                num -= k0;
+                num += num2;
+                num2 = num + 56u;
+                num *= (num2 & 239u);
+                b[i] = (byte)num;
+                num >>= 1;
+            }
+        }
+
+        // Token: 0x0600003A RID: 58 RVA: 0x000036C8 File Offset: 0x00001AC8
+        private void dd(byte[] b, int L, uint k)
+        {
+            byte[] array = new byte[256];
+            this.gk(array, k);
+            for (int i = 0; i < L; i++)
+            {
+                byte b2 = b[i];
+                b2 ^= array[i % 253];
+                b2 += 3;
+                b2 += array[i % 89];
+                b2 ^= 153;
+                b[i] = b2;
+            }
+        }
+
+        // Token: 0x0600003B RID: 59 RVA: 0x00003728 File Offset: 0x00001B28
+        public byte[] Data(string fn)
+        {
+            PRead.fe fe;
+            if (!this.ti.TryGetValue(fn, out fe))
+            {
+                return null;
+            }
+            this.fs.Position = (long)((ulong)fe.p);
+            byte[] array = new byte[fe.L];
+            this.fs.Read(array, 0, array.Length);
+            this.dd(array, array.Length, fe.k);
+            return array;
+        }
+
+        // Token: 0x04000032 RID: 50
+        private FileStream fs;
+
+        // Token: 0x04000033 RID: 51
+        public Dictionary<string, PRead.fe> ti;
+
+        // Token: 0x0200000B RID: 11
+        public struct fe
+        {
+            // Token: 0x04000034 RID: 52
+            public uint p;
+
+            // Token: 0x04000035 RID: 53
+            public uint L;
+
+            // Token: 0x04000036 RID: 54
+            public uint k;
+        }
+    }
+}
